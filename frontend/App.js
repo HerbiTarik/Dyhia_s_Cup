bb70bcf1b8e5bb39dbb40a4c5d1b1fd6afd51bc8
Herbi <herbitarik2509@gmail.com>
2025-01-02 06:35:19 +0100

    bootsplash react native added at application startup

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,13 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import './global.css';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true, duration: 500}); // Cache le splash screen
+  }, []);
+
   return (
     <View>
       <Text className="bg-slate-500 text-center">App</Text>
